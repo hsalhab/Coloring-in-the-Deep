eac8d5e95b3fc585227a8b94d9e7ccaf8808dc80
hsalhab <husam_salhab@brown.edu>
2019-12-08 21:03:07 -0500

    sequentialized model

--- model.py
+++ model.py
@@ -12,47 +12,49 @@ class IC_Model(tf.keras.Model):
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=hp.LEARNING_RATE)
         self.batch_size = hp.BATCH_SIZE
 
-        self.conv1_1 = Conv2D(64, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv1_2 = Conv2D(64, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', strides=(2, 2))
-        self.batch1 = BatchNormalization()
+        self.model = tf.keras.Sequential([
+            Conv2D(64, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(64, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', strides=(2, 2)),
+            BatchNormalization(),
 
-        self.conv2_1 = Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv2_2 = Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', strides=(2, 2))
-        self.batch2 = BatchNormalization()
+            Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', strides=(2, 2)),
+            BatchNormalization(),
 
-        self.conv3_1 = Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv3_2 = Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv3_3 = Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', strides=(2, 2))
-        self.batch3 = BatchNormalization()
+            Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', strides=(2, 2)),
+            BatchNormalization(),
 
-        self.conv4_1 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv4_2 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv4_3 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.batch4 = BatchNormalization()
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            BatchNormalization(),
 
-        self.conv5_1 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2)
-        self.conv5_2 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2)
-        self.conv5_3 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2)
-        self.batch5 = BatchNormalization()
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2),
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2),
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2),
+            BatchNormalization(),
 
-        self.conv6_1 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2)
-        self.conv6_2 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2)
-        self.conv6_3 = Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2)
-        self.batch6 = BatchNormalization()
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2),
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2),
+            Conv2D(512, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same', dilation_rate=2),
+            BatchNormalization(),
 
-        self.conv7_1 = Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv7_2 = Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv7_3 = Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.batch7 = BatchNormalization()
+            Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(256, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            BatchNormalization(),
 
-        self.upsample = UpSampling2D(size=(2, 2))
-        self.conv8_1 = Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv8_2 = Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.conv8_3 = Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same')
-        self.batch8 = BatchNormalization()
+            UpSampling2D(size=(2, 2)),
+            Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            Conv2D(128, kernel_size=hp.KERNEL_SIZE, activation='relu', padding='same'),
+            BatchNormalization(),
 
-        self.final_upsample = UpSampling2D(size=(4, 4))
-        self.final = Conv2D(hp.NUM_CLASSES, kernel_size=1, padding='same')
+            UpSampling2D(size=(4, 4)),
+            Conv2D(hp.NUM_CLASSES, kernel_size=1, padding='same')
+        ])
 
 
     @tf.function
@@ -63,48 +65,7 @@ class IC_Model(tf.keras.Model):
         :return:
         """
 
-        hidden = self.conv1_1(inputs)
-        hidden = self.conv1_2(hidden)
-        hidden = self.batch1(hidden)
-
-        hidden = self.conv2_1(hidden)
-        hidden = self.conv2_2(hidden)
-        hidden = self.batch2(hidden)
-
-        hidden = self.conv3_1(hidden)
-        hidden = self.conv3_2(hidden)
-        hidden = self.conv3_3(hidden)
-        hidden = self.batch3(hidden)
-
-        hidden = self.conv4_1(hidden)
-        hidden = self.conv4_2(hidden)
-        hidden = self.conv4_3(hidden)
-        hidden = self.batch4(hidden)
-
-        hidden = self.conv5_1(hidden)
-        hidden = self.conv5_2(hidden)
-        hidden = self.conv5_3(hidden)
-        hidden = self.batch5(hidden)
-
-        hidden = self.conv6_1(hidden)
-        hidden = self.conv6_2(hidden)
-        hidden = self.conv6_3(hidden)
-        hidden = self.batch6(hidden)
-
-        hidden = self.conv7_1(hidden)
-        hidden = self.conv7_2(hidden)
-        hidden = self.conv7_3(hidden)
-        hidden = self.batch7(hidden)
-
-        hidden = self.upsample(hidden)
-        hidden = self.conv8_1(hidden)
-        hidden = self.conv8_2(hidden)
-        hidden = self.conv8_3(hidden)
-        hidden = self.batch8(hidden)
-
-        upsampled = self.final_upsample(hidden)
-        logits = self.final(upsampled)
-
+        logits = self.model.call(inputs)
         return logits
 
 
