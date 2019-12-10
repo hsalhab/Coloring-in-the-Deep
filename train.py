badf16f9cc89367bc3f0554ffe6ddf2f4022cd4a
hsalhab <husam_salhab@brown.edu>
2019-12-10 17:24:11 -0500

    updated epochs

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ if testing:
     test(model, encoder)
 else:
     # checkpoint.restore(manager.latest_checkpoint)
-    epochs = 50
+    epochs = 1000
     for i in range(epochs):
         print("epoch {} out of {}".format(i, epochs))
         shuffle_data()
