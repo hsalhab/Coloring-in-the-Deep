d3df3effe2b36ef08af33b6bdd5e135e727e2ece
hsalhab <husam_salhab@brown.edu>
2019-12-09 21:51:13 -0500

    added color rebalancing

--- encoder.py
+++ encoder.py
@@ -6,7 +6,8 @@ import cv2
 
 class Encoder(object):
     def __init__(self):
-        self.bin_centers = np.load("bin_centers.npy")
+        self.bin_centers = np.load("resources/bin_centers.npy")
+        self.prior_weights = np.load("resources/weights.npy")
         self.knn = nn.NearestNeighbors(
             n_neighbors=5, algorithm='ball_tree').fit(self.bin_centers)
         self.pixel_idx = np.arange(IMAGE_HEIGHT * IMAGE_WIDTH)[:, np.newaxis]
@@ -25,6 +26,24 @@ class Encoder(object):
         label = np.reshape(label, (IMAGE_HEIGHT, IMAGE_WIDTH, 313))
 
         return label
+
+    def soft_encode_with_color_rebal(self, img):
+        label = np.zeros((IMAGE_HEIGHT * IMAGE_WIDTH, 313))
+        img = np.reshape(img, (-1, 2))
+        distances, indices = self.knn.kneighbors(img, 5)
+
+        weights = np.exp(-distances ** 2 / (2 * SIGMA ** 2))
+        weights = weights / np.sum(weights, axis=1)[:, np.newaxis]
+
+        label[self.pixel_idx, indices] = weights
+        label = np.reshape(label.T, (313, IMAGE_HEIGHT, IMAGE_WIDTH))
+
+        rebal_idx = indices[:, 0]
+        rebal_weights = self.prior_weights[rebal_idx]
+        rebal_weights = np.reshape(rebal_weights, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        rebal_label = rebal_weights * label
+
+        return rebal_label
 
     def decode(self, logits, l_imgs, ab_imgs):
         for i in range(ab_imgs.shape[0]):
